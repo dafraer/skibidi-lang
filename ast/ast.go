e856c737e7312b59c9ef6de7dae10037903a3360
Kamil <kdnuriev@gmail.com>
2024-06-15 22:32:19 +0400

    Added expression statements and String() methods

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "skibidilang/token"
+import (
+	"bytes"
+	"skibidilang/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -39,15 +43,61 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
 type Identifier struct {
 	Token token.Token // the token.IDENT token
 	Value string
 }
 
-// important useless dummy methods
-func (i *Identifier) expressionNode()            {}
-func (i *Identifier) TokenLiteral() string       { return i.Token.Literal }
-func (rs *ReturnStatement) statementNode()       {}
-func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
-func (ls *LetStatement) statementNode()          {}
-func (ls *LetStatement) TokenLiteral() string    { return ls.Token.Literal }
+// Important useless dummy methods for making structs implement interfaces
+func (i *Identifier) expressionNode()                {}
+func (i *Identifier) TokenLiteral() string           { return i.Token.Literal }
+func (rs *ReturnStatement) statementNode()           {}
+func (rs *ReturnStatement) TokenLiteral() string     { return rs.Token.Literal }
+func (ls *LetStatement) statementNode()              {}
+func (ls *LetStatement) TokenLiteral() string        { return ls.Token.Literal }
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+// String methods
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+func (i *Identifier) String() string { return i.Value }
